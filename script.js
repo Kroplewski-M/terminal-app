40b847e3130ea8b0e4c92fb45e510cbfbc0ef9dd
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2022-09-01 11:46:48 +0100

    started on intro

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const terminal = document.querySelector("#terminal");
+
+
 const commands={
     banner : `
     <pre class="text-white">     
@@ -12,15 +15,42 @@ const commands={
 /:::=======:::\`\`\
  """""""""""""  
 </pre>
-<br><br>
+<br>
+    `
+};
+const messages={
+     introMessage : 
+     `<p class="text-white font-mono">Mats Terminal (MT) Not A Corperation.</p>`,
+
+    inputElements :
+    `
+    <p class="text-green-400">visitor@mat'sTerminal:$</p>
+    <input type="text" autofocus class="pl-[5px] text-orange-300 bg-transparent font-mono">
+    `,
+    helpMessage:
+    `
+        <p class="text-gray-300 font-mono">Type <span class="text-orange-400"> help </span> For Command List</p>
+        <br><br>
     `
 }
 
 
-
 function init(){
-    let htmlObject = document.createElement('div');
-    htmlObject.innerHTML = commands.banner;
-    terminal.prepend(htmlObject);
+    let help = document.createElement('div');
+    help.innerHTML = messages.helpMessage;
+    terminal.prepend(help);
+
+    let banner = document.createElement('div');
+    banner.innerHTML = commands.banner;
+    terminal.prepend(banner);
+
+    let intro = document.createElement('div');
+    intro.innerHTML = messages.introMessage;
+    terminal.prepend(intro);
+
+    let input = document.createElement('div');
+    input.classList.add("flex");
+    input.innerHTML = messages.inputElements;
+    terminal.appendChild(input);
 }
 init();
