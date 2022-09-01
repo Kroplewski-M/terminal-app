ecfb9c2eaaa736f9ea5fa398a39fee9697645a72
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2022-09-01 13:58:56 +0100

    added priting to dom function- makes it cleaner

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const terminal = document.querySelector("#terminal");
-
+const help = "Type <span class='text-orange-400'> help </span> For Command List";
+const introMessage = "Mats Terminal (MT) Not A Corperation.";
 
 const commands={
     banner : `
@@ -18,33 +19,27 @@ const commands={
 };
 const messages={
      introMessage : 
-     `<p class="text-white font-mono">Mats Terminal (MT) Not A Corperation.</p>`,
+     `<p class="text-white font-mono"></p>`,
 
     inputElements :
     `
     <p class="text-green-400">visitor<span class="text-orange-400">@</span>mat'sTerminal:~$</p>
     <input type="text" autofocus class="pl-[5px] text-orange-300 bg-transparent font-mono">
     `,
-    helpMessage:
-    `
-        <p class="text-gray-300 font-mono">Type <span class="text-orange-400"> help </span> For Command List</p>
-        <br><br>
-    `
+};
+
+function printToDOM(message,color){
+    let print = `<p class="font-mono text-${color}">${message}</p>`
+    let dom = document.createElement('div');
+    dom.innerHTML = print;
+    terminal.prepend(dom);
 }
 
 
 function init(){
-    let help = document.createElement('div');
-    help.innerHTML = messages.helpMessage;
-    terminal.prepend(help);
-
-    let banner = document.createElement('div');
-    banner.innerHTML = commands.banner;
-    terminal.prepend(banner);
-
-    let intro = document.createElement('div');
-    intro.innerHTML = messages.introMessage;
-    terminal.prepend(intro);
+    printToDOM(help,"gray-400");
+    printToDOM(commands.banner)
+    printToDOM(introMessage,"white")
 
     let input = document.createElement('div');
     input.classList.add("flex");
